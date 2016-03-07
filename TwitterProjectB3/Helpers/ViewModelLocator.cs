91758bf14c6dd39656b346d442c9fe9fc6947a53
Camille COLVRAY <camille.colvray@ynov.com>
2016-03-07 13:55:11 +0100

    V1

---
+++ TwitterProjectB3/Helpers/ViewModelLocator.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Practices.Unity;
+using Microsoft.Practices.Unity.InterceptionExtension;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitterProjectB3.ViewModel;
+
+namespace TwitterProjectB3.Helpers
+{
+    public class ViewModelLocator
+    {
+        static IUnityContainer MainContainer;
+
+        static ViewModelLocator()
+        {
+            MainContainer = new UnityContainer();
+            MainContainer.AddNewExtension<Interception>();
+            MainContainer.RegisterType<MainViewModel>().Configure<Interception>().SetInterceptorFor<MainViewModel>(new VirtualMethodInterceptor());
+
+        }
+
+        public MainViewModel Main
+        {
+            get { return MainContainer.Resolve<MainViewModel>(); }
+        }
+    }
+}
